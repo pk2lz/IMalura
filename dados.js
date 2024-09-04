331ed2027d9b5c5ebefc9480e9919ceb19cdbb24
Patriky Santana <whiteexc@gmail.com>
2024-09-04 16:09:35 -0300

    first commit

---
+++ dados.js
@@ -0,0 +1,25 @@
+let dados = [
+  {
+    titulo: "Talon",
+    descricao:
+      "Forjado nas ruas cruéis de Noxus, ele sobreviveu graças à sua astúcia e à lâmina afiada. Adotado pela influente família Du Couteau, Talon agora serve ao império, eliminando ameaças com precisão e eficiência.",
+    link: "https://www.leagueoflegends.com/pt-br/champions/talon/",
+    builds: "https://www.mobafire.com/league-of-legends/champion/talon-82",
+  },
+
+  {
+    titulo: "Darius",
+    descricao:
+      "O Grande Martelo de Noxus, Darius é um general implacável que inspira terror em seus inimigos. Com sua força bruta e sede de sangue, ele é uma força a ser considerada em qualquer batalha.",
+    link: "https://www.leagueoflegends.com/pt-br/champions/darius/",
+    builds: "https://www.mobafire.com/league-of-legends/champion/darius-98",
+  },
+
+  {
+    titulo: "Sion",
+    descricao:
+      "Sion é um monstro indestrutível criado por magos noxianos. Sem memória de sua vida passada, ele agora serve como uma arma de guerra, marchando implacavelmente em direção à batalha.",
+    link: "https://www.leagueoflegends.com/pt-br/champions/sion/",
+    builds: "https://www.mobafire.com/league-of-legends/champion/sion-6",
+  },
+];
