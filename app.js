fd251741fce0f4ea2f7ca6e52c1b2b2e8a94f2f1
Patriky Santana <whiteexc@gmail.com>
2024-09-05 14:53:31 -0300

    aula4

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log (dados);
+function pesquisar() {
+    for (let dado of dados) {
+        resultados += `
+      <div class="item-resultado">
+                <h2>
+                  <a
+                    href="${dado.link}"
+                    target="_blank"
+                    >${dado.titulo}
+                    </a>
+                </h2>
+                <p class="descricao-meta">
+                ${dado.descricao}
+                
+                </p>
+              </div>
+      `;
+      }
+      
+      section.innerHTML = resultados
+}
+
+// console.log (dados);
+
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+let resultados = ""
+
+// para cada dentro da lista de dados
+
